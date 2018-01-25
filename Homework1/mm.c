26954d5d6ae807de02ad74cbbddc40f6805692f0
tylern4 <tylern@canisius.edu>
2018-01-25 09:49:19 -0500

    Maybe working mm

--- Homework1/mm.c
+++ Homework1/mm.c
@@ -3,7 +3,6 @@
 #include <math.h>
 #include <string.h>
 #include <sys/timeb.h>
-#include <omp.h>
 
 /* read timer in second */
 double read_timer() {
@@ -30,98 +29,118 @@ void init(REAL A[], int N) {
   }
 }
 
-int num_ths = 4;
-
-void mm(int N, int K, int M, REAL *A, REAL *B, REAL *C);
-void mm_omp_parallel(int N, int K, int M, REAL *A, REAL *B, REAL *C);
-void mm_omp_parallel_for(int N, int K, int M, REAL *A, REAL *B, REAL *C);
+/* C[N][M] = A[N][K] * B[K][M] */
+void mm(int N, int K, int M, REAL *A, REAL *B, REAL *C, int A_rowMajor,
+        int B_rowMajor);
 
 /**
- * To compile: gcc mm.c -fopenmp -o mm
+ * To compile: gcc mm.c -o mm
  */
 int main(int argc, char *argv[]) {
   int N = VECTOR_LENGTH;
   int M = N;
   int K = N;
   double elapsed; /* for timing */
-  if (argc < 5) {
-    fprintf(stderr, "Usage: mm [<N(%d)>] <K(%d) [<M(%d)>] [<num_threads(%d)]\n",
-            N, K, M, num_ths);
-    fprintf(stderr, "\t Example: ./mm %d %d %d %d (default)\n", N, K, M,
-            num_ths);
+  if (argc < 4) {
+    fprintf(stderr, "Usage: mm [<N(%d)>] [<K(%d)>] [<M(%d)>] \n", N, K, M);
+    fprintf(stderr, "\n\tC[N][M] = A[N][K] * B[K][M]\n");
+    fprintf(stderr, "\n\tExample: ./mm %d %d %d (default)\n", N, K, M);
   } else {
     N = atoi(argv[1]);
     K = atoi(argv[2]);
     M = atoi(argv[3]);
-    num_ths = atoi(argv[4]);
   }
-  //    printf("\tC[%d][%d] = A[%d][%d] * B[%d][%d] with %d threads\n", N, M, N,
-  //    K, K, M, num_ths);
+
   REAL *A = malloc(sizeof(REAL) * N * K);
   REAL *B = malloc(sizeof(REAL) * K * M);
   REAL *C = malloc(sizeof(REAL) * N * M);
+  REAL *D = malloc(sizeof(REAL) * N);
 
   srand48((1 << 12));
   init(A, N * K);
   init(B, K * M);
 
   /* example run */
-  double elapsed_mm = read_timer();
-  mm(N, K, M, A, B, C);
-  elapsed_mm = (read_timer() - elapsed_mm);
+  double elapsed_mm_RR = read_timer();
+  mm(N, K, M, A, B, C, 1, 1);
+  elapsed_mm_RR = (read_timer() - elapsed_mm_RR);
 
-  double elapsed_mm_omp_parallel = read_timer();
-  mm_omp_parallel(N, K, M, A, B, C);
-  elapsed_mm_omp_parallel = (read_timer() - elapsed_mm_omp_parallel);
+  double elapsed_mm_RC = read_timer();
+  mm(N, K, M, A, B, C, 1, 0);
+  elapsed_mm_RC = (read_timer() - elapsed_mm_RC);
 
-  double elapsed_mm_omp_parallel_for = read_timer();
-  mm_omp_parallel_for(N, K, M, A, B, C);
-  elapsed_mm_omp_parallel_for = (read_timer() - elapsed_mm_omp_parallel_for);
+  double elapsed_mm_CR = read_timer();
+  mm(N, K, M, A, B, C, 0, 1);
+  elapsed_mm_CR = (read_timer() - elapsed_mm_CR);
+
+  double elapsed_mm_CC = read_timer();
+  mm(N, K, M, A, B, C, 0, 0);
+  elapsed_mm_CC = (read_timer() - elapsed_mm_CC);
 
   /* you should add the call to each function and time the execution */
-  printf("====================================================================="
-         "=================================\n");
-  printf("\tC[%d][%d] = A[%d][%d] * B[%d][%d] with %d threads for OpenMP\n", N,
-         M, N, K, K, M, num_ths);
-  printf("---------------------------------------------------------------------"
-         "---------------------------------\n");
+  printf("=================================================================\n");
+  printf("\tC[%d][%d] = A[%d][%d] * B[%d][%d]\n", N, M, N, K, K, M);
+  printf("-----------------------------------------------------------------\n");
   printf("Performance:\t\t\tRuntime (ms)\t MFLOPS \n");
-  printf("---------------------------------------------------------------------"
-         "---------------------------------\n");
-  printf("mm:\t\t\t\t%4f\t%4f\n", elapsed_mm * 1.0e3,
-         M * N * K / (1.0e6 * elapsed_mm));
-  printf("mm_omp_parallel:\t\t%4f\t%4f\n", elapsed_mm_omp_parallel * 1.0e3,
-         M * N * K / (1.0e6 * elapsed_mm_omp_parallel));
-  printf("mm_omp_paralell_for:\t\t%4f\t%4f\n", elapsed_mm_omp_parallel_for * 1.0e3,
-         M * N * K / (1.0e6 * elapsed_mm_omp_parallel_for));
-
+  printf("-----------------------------------------------------------------\n");
+  printf("mm row row:\t\t\t\t%4f\t%4f\n", elapsed_mm_RR * 1.0e3,
+         M * N * K / (1.0e6 * elapsed_mm_RR));
+  printf("mm row col:\t\t\t\t%4f\t%4f\n", elapsed_mm_RC * 1.0e3,
+         M * N * K / (1.0e6 * elapsed_mm_RC));
+  printf("mm col row:\t\t\t\t%4f\t%4f\n", elapsed_mm_CR * 1.0e3,
+         M * N * K / (1.0e6 * elapsed_mm_CR));
+  printf("mm col col:\t\t\t\t%4f\t%4f\n", elapsed_mm_CC * 1.0e3,
+         M * N * K / (1.0e6 * elapsed_mm_CC));
   free(A);
   free(B);
   free(C);
   return 0;
 }
 
-void mm(int N, int K, int M, REAL *A, REAL *B, REAL *C) {
+void mm(int N, int K, int M, REAL *A, REAL *B, REAL *C, int A_rowMajor,
+        int B_rowMajor) {
   int i, j, w;
-  for (i = 0; i < N; i++)
-    for (j = 0; j < M; j++) {
-      REAL temp = 0.0;
-      for (w = 0; w < K; w++)
-        temp += A[i * K + w] * B[w * M + j];
-      C[i * M + j] = temp;
-    }
-}
-
-void mm_omp_parallel(int N, int K, int M, REAL *A, REAL *B, REAL *C) {}
-
-void mm_omp_parallel_for(int N, int K, int M, REAL *A, REAL *B, REAL *C) {
-  int i, j, w;
-#pragma omp parallel for private (i, j, w) num_threads(num_ths)
-  for (i = 0; i < N; i++)
-    for (j = 0; j < M; j++) {
-      REAL temp = 0.0;
-      for (w = 0; w < K; w++)
-        temp += A[i * K + w] * B[w * M + j];
-      C[i * M + j] = temp;
-    }
+  // Both rowmajor
+  if (A_rowMajor > 0 && B_rowMajor > 0) {
+    for (i = 0; i < N; i++)
+      for (j = 0; j < M; j++) {
+        REAL temp = 0.0;
+        for (w = 0; w < K; w++)
+          temp += A[i * K + w] * B[w * M + j];
+        C[i * M + j] = temp;
+      }
+  }
+  // A columbmajor B rowmajor
+  // swap N <-> K
+  else if (A_rowMajor == 0 && B_rowMajor > 0) {
+    for (i = 0; i < K; i++)
+      for (j = 0; j < M; j++) {
+        REAL temp = 0.0;
+        for (w = 0; w < N; w++)
+          temp += A[i * N + w] * B[w * M + j];
+        C[i * M + j] = temp;
+      }
+  }
+  // A rowmajor B columbmajor
+  // swap M <-> K
+  else if (A_rowMajor > 0 && B_rowMajor == 0) {
+    for (i = 0; i < N; i++)
+      for (j = 0; j < K; j++) {
+        REAL temp = 0.0;
+        for (w = 0; w < M; w++)
+          temp += A[i * K + w] * B[w * K + j];
+        C[i * M + j] = temp;
+      }
+  }
+  // Both columbmajor
+  // ???
+  else if (A_rowMajor == 0 && B_rowMajor == 0) {
+    for (i = 0; i < N; i++)
+      for (j = 0; j < M; j++) {
+        REAL temp = 0.0;
+        for (w = 0; w < K; w++)
+          temp += A[i * K + w] * B[w * M + j];
+        C[i * M + j] = temp;
+      }
+  }
 }
