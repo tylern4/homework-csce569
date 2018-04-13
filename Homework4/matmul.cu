0318d45958e93b0cce6e64cc302120c1235746ca
tylern4 <tylern@canisius.edu>
2018-04-13 14:21:42 -0400

    Test on bridges

--- Homework4/matmul.cu
+++ Homework4/matmul.cu
@@ -59,11 +59,11 @@ void matmul_openmp(int N, REAL *A, REAL *B, REAL *C, int num_tasks);
 void matmul_cuda_v1_vanilla(int N, REAL *A, REAL *B, REAL *C);
 void matmul_cuda_v2_shmem(int N, REAL *A, REAL *B, REAL *C);
 void matmul_cuda_v3_cublas(int N, REAL *A, REAL *B, REAL *C);
-void cublas_v3_1(int N, REAL *A, REAL *B, REAL *C);
 
 int main(int argc, char *argv[]) {
   int N;
   int num_tasks = 5; /* 5 is default number of tasks */
+  int task = 0;
   double elapsed_base, elapsed_openmp;
   double elapsed_cuda_v1, elapsed_cuda_v2, elapsed_cuda_v3; /* for timing */
   if (argc < 2) {
@@ -73,7 +73,10 @@ int main(int argc, char *argv[]) {
   N = atoi(argv[1]);
   if (argc > 2)
     num_tasks = atoi(argv[2]);
-  REAL *heap_buffer = (REAL *)malloc(sizeof(REAL) * N * N * 8);
+  if (argc > 3)
+    task = atoi(argv[3]);
+
+  REAL *heap_buffer = (REAL *)malloc(sizeof(REAL) * N * N * 7);
   /* we use 5 matrix in this example */
   /* below is a cast from memory buffer to a 2-d row-major array */
   REAL *A = heap_buffer;
@@ -87,56 +90,128 @@ int main(int argc, char *argv[]) {
   srand48((1 << 12));
   init(N, N, A);
   init(N, N, B);
+  switch (task) {
+  case 0:
+    /* example run */
+    elapsed_base = read_timer();
+    matmul_base(N, A, B, C_base);
+    elapsed_base = (read_timer() - elapsed_base);
 
-  /* example run */
-  elapsed_base = read_timer();
-  matmul_base(N, A, B, C_base);
-  elapsed_base = (read_timer() - elapsed_base);
+    elapsed_openmp = read_timer();
+    matmul_openmp(N, A, B, C_openmp, num_tasks);
+    elapsed_openmp = (read_timer() - elapsed_openmp);
 
-  elapsed_openmp = read_timer();
-  matmul_openmp(N, A, B, C_openmp, num_tasks);
-  elapsed_openmp = (read_timer() - elapsed_openmp);
+    /* call and timing for the three CUDA versions */
+    // TODO: call and time for
+    elapsed_cuda_v1 = read_timer();
+    matmul_cuda_v1_vanilla(N, A, B, C_v1);
+    elapsed_cuda_v1 = (read_timer() - elapsed_cuda_v1);
 
-  /* call and timing for the three CUDA versions */
-  // TODO: call and time for
-  elapsed_cuda_v1 = read_timer();
-  matmul_cuda_v1_vanilla(N, A, B, C_v1);
-  elapsed_cuda_v1 = (read_timer() - elapsed_cuda_v1);
+    // TODO: call and time for matmul_cuda_v1_shmem(int N, REAL *A, REAL *B,
+    // REAL*C);
+    elapsed_cuda_v2 = read_timer();
+    matmul_cuda_v2_shmem(N, A, B, C_v2);
+    elapsed_cuda_v2 = (read_timer() - elapsed_cuda_v2);
 
-  // TODO: call and time for matmul_cuda_v1_shmem(int N, REAL *A, REAL *B, REAL
-  // *C);
-  elapsed_cuda_v2 = read_timer();
-  cublas_v3_1(N, A, B, C_v2);
-  // matmul_cuda_v2_shmem(N, A, B, C_v2);
-  elapsed_cuda_v2 = (read_timer() - elapsed_cuda_v2);
+    // TODO: call and time for matmul_cuda_v1_cublas(int N, REAL *A, REAL *B,
+    // REAL
+    // *C);
+    elapsed_cuda_v3 = read_timer();
+    matmul_cuda_v3_cublas(N, A, B, C_v3);
+    elapsed_cuda_v3 = (read_timer() - elapsed_cuda_v3);
 
-  // TODO: call and time for matmul_cuda_v1_cublas(int N, REAL *A, REAL *B, REAL
-  // *C);
-  elapsed_cuda_v3 = read_timer();
-  matmul_cuda_v3_cublas(N, A, B, C_v3);
-  elapsed_cuda_v3 = (read_timer() - elapsed_cuda_v3);
+    printf("===============================================================\n");
+    printf("Matrix Multiplication: A[M][K] * B[k][N] = C[M][N]\n");
+    printf("\tM=K=N=%d, %d threads/tasks\n", N, num_tasks);
+    printf("---------------------------------------------------------------\n");
+    printf("Performance:\tRuntime (ms)\t MFLOPS\t\tError\n");
+    printf("---------------------------------------------------------------\n");
+    printf("matmul_base:\t%4f\t%4f\t%g\n", elapsed_base * 1.0e3,
+           ((((2.0 * N) * N) * N) / (1.0e6 * elapsed_base)),
+           maxerror(N, N, C_base, C_base));
+    printf("matmul_openmp:\t%4f\t%4f\t%g\n", elapsed_openmp * 1.0e3,
+           ((((2.0 * N) * N) * N) / (1.0e6 * elapsed_openmp)),
+           maxerror(N, N, C_base, C_openmp));
+    printf("matmul_cuda_v1:\t%4f\t%4f\t%g\n", elapsed_cuda_v1 * 1.0e3,
+           ((((2.0 * N) * N) * N) / (1.0e6 * elapsed_cuda_v1)),
+           maxerror(N, N, C_base, C_v1));
+    printf("matmul_cuda_v2:\t%4f\t%4f\t%g\n", elapsed_cuda_v2 * 1.0e3,
+           ((((2.0 * N) * N) * N) / (1.0e6 * elapsed_cuda_v2)),
+           maxerror(N, N, C_base, C_v2));
+    printf("matmul_cuda_v3:\t%4f\t%4f\t%g\n", elapsed_cuda_v3 * 1.0e3,
+           ((((2.0 * N) * N) * N) / (1.0e6 * elapsed_cuda_v3)),
+           maxerror(N, N, C_base, C_v3));
+    break;
+  case 1:
+    /* example run */
+    elapsed_base = read_timer();
+    matmul_base(N, A, B, C_base);
+    elapsed_base = (read_timer() - elapsed_base);
+    printf("===============================================================\n");
+    printf("Matrix Multiplication: A[M][K] * B[k][N] = C[M][N]\n");
+    printf("\tM=K=N=%d, %d threads/tasks\n", N, num_tasks);
+    printf("---------------------------------------------------------------\n");
+    printf("Performance:\tRuntime (ms)\t MFLOPS\n");
+    printf("---------------------------------------------------------------\n");
+    printf("matmul_base:\t%4f\t%4f\n", elapsed_base * 1.0e3,
+           ((((2.0 * N) * N) * N) / (1.0e6 * elapsed_base)));
+    break;
+  case 2:
+    elapsed_openmp = read_timer();
+    matmul_openmp(N, A, B, C_openmp, num_tasks);
+    elapsed_openmp = (read_timer() - elapsed_openmp);
+    printf("===============================================================\n");
+    printf("Matrix Multiplication: A[M][K] * B[k][N] = C[M][N]\n");
+    printf("\tM=K=N=%d, %d threads/tasks\n", N, num_tasks);
+    printf("---------------------------------------------------------------\n");
+    printf("Performance:\tRuntime (ms)\t MFLOPS\n");
+    printf("---------------------------------------------------------------\n");
+    printf("matmul_openmp:\t%4f\t%4f\n", elapsed_openmp * 1.0e3,
+           ((((2.0 * N) * N) * N) / (1.0e6 * elapsed_openmp)));
+    break;
+  case 3:
+    elapsed_cuda_v1 = read_timer();
+    matmul_cuda_v1_vanilla(N, A, B, C_v1);
+    elapsed_cuda_v1 = (read_timer() - elapsed_cuda_v1);
+    printf("===============================================================\n");
+    printf("Matrix Multiplication: A[M][K] * B[k][N] = C[M][N]\n");
+    printf("\tM=K=N=%d, %d threads/tasks\n", N, num_tasks);
+    printf("---------------------------------------------------------------\n");
+    printf("Performance:\tRuntime (ms)\t MFLOPS\n");
+    printf("---------------------------------------------------------------\n");
+    printf("matmul_cuda_v1:\t%4f\t%4f\n", elapsed_cuda_v1 * 1.0e3,
+           ((((2.0 * N) * N) * N) / (1.0e6 * elapsed_cuda_v1)));
+    break;
+  case 4:
+    // TODO: call and time for matmul_cuda_v1_shmem(int N, REAL *A, REAL *B,
+    // REAL*C);
+    elapsed_cuda_v2 = read_timer();
+    matmul_cuda_v2_shmem(N, A, B, C_v2);
+    elapsed_cuda_v2 = (read_timer() - elapsed_cuda_v2);
+    printf("===============================================================\n");
+    printf("Matrix Multiplication: A[M][K] * B[k][N] = C[M][N]\n");
+    printf("\tM=K=N=%d, %d threads/tasks\n", N, num_tasks);
+    printf("---------------------------------------------------------------\n");
+    printf("Performance:\tRuntime (ms)\t MFLOPS\n");
+    printf("---------------------------------------------------------------\n");
+    printf("matmul_cuda_v2:\t%4f\t%4f\n", elapsed_cuda_v2 * 1.0e3,
+           ((((2.0 * N) * N) * N) / (1.0e6 * elapsed_cuda_v2)));
+    break;
+  case 5:
+    elapsed_cuda_v3 = read_timer();
+    matmul_cuda_v3_cublas(N, A, B, C_v3);
+    elapsed_cuda_v3 = (read_timer() - elapsed_cuda_v3);
+    printf("===============================================================\n");
+    printf("Matrix Multiplication: A[M][K] * B[k][N] = C[M][N]\n");
+    printf("\tM=K=N=%d, %d threads/tasks\n", N, num_tasks);
+    printf("---------------------------------------------------------------\n");
+    printf("Performance:\tRuntime (ms)\t MFLOPS\n");
+    printf("---------------------------------------------------------------\n");
+    printf("matmul_cuda_v3:\t%4f\t%4f\n", elapsed_cuda_v3 * 1.0e3,
+           ((((2.0 * N) * N) * N) / (1.0e6 * elapsed_cuda_v3)));
+    break;
+  }
 
-  printf("===============================================================\n");
-  printf("Matrix Multiplication: A[M][K] * B[k][N] = C[M][N]\n");
-  printf("\tM=K=N=%d, %d threads/tasks\n", N, num_tasks);
-  printf("---------------------------------------------------------------\n");
-  printf("Performance:\tRuntime (ms)\t MFLOPS\t\tError\n");
-  printf("---------------------------------------------------------------\n");
-  printf("matmul_base:\t%4f\t%4f\t%g\n", elapsed_base * 1.0e3,
-         ((((2.0 * N) * N) * N) / (1.0e6 * elapsed_base)),
-         maxerror(N, N, C_base, C_base));
-  printf("matmul_openmp:\t%4f\t%4f\t%g\n", elapsed_openmp * 1.0e3,
-         ((((2.0 * N) * N) * N) / (1.0e6 * elapsed_openmp)),
-         maxerror(N, N, C_base, C_openmp));
-  printf("matmul_cuda_v1:\t%4f\t%4f\t%g\n", elapsed_cuda_v1 * 1.0e3,
-         ((((2.0 * N) * N) * N) / (1.0e6 * elapsed_cuda_v1)),
-         maxerror(N, N, C_base, C_v1));
-  printf("matmul_cuda_v2:\t%4f\t%4f\t%g\n", elapsed_cuda_v2 * 1.0e3,
-         ((((2.0 * N) * N) * N) / (1.0e6 * elapsed_cuda_v2)),
-         maxerror(N, N, C_base, C_v2));
-  printf("matmul_cuda_v3:\t%4f\t%4f\t%g\n", elapsed_cuda_v3 * 1.0e3,
-         ((((2.0 * N) * N) * N) / (1.0e6 * elapsed_cuda_v3)),
-         maxerror(N, N, C_base, C_v3));
   free(heap_buffer);
   return 0;
 }
@@ -240,7 +315,7 @@ __device__ REAL *GetSubMatrix(REAL *A, int row, int col, int N) {
 }
 
 __global__ void matmul_cuda_v2_shmem_kernel(int N, REAL *A, REAL *B, REAL *C) {
-  /*
+
   // Block row and column
   int blockRow = blockIdx.y;
   int blockCol = blockIdx.x;
@@ -290,7 +365,6 @@ __global__ void matmul_cuda_v2_shmem_kernel(int N, REAL *A, REAL *B, REAL *C) {
   // Write Csub to device memory
   // Each thread writes one element
   SetElement(C_sub, row, col, Cvalue, N);
-  */
 }
 /*
  * call to kernel that use GPU shared memory
@@ -331,41 +405,6 @@ void matmul_cuda_v2_shmem(int N, REAL *A, REAL *B, REAL *C) {
  * call to sgemm of cublas library
  */
 void matmul_cuda_v3_cublas(int N, REAL *A, REAL *B, REAL *C) {
-  int size = N * N * sizeof(REAL);
-  REAL alpha = 1.0f;
-  REAL beta = 0.0f;
-
-  // Make cuda matricies
-  REAL *cuda_A = (REAL *)malloc(size);
-  REAL *cuda_B = (REAL *)malloc(size);
-  REAL *cuda_C = (REAL *)malloc(size);
-
-  // Copy A to cuda memory
-  cudaMalloc(&cuda_A, size);
-  cudaMemcpy(cuda_A, A, size, cudaMemcpyHostToDevice);
-
-  // Copy B to cuda memory
-  cudaMalloc(&cuda_B, size);
-  cudaMemcpy(cuda_B, B, size, cudaMemcpyHostToDevice);
-
-  // Allocate C to cuda memory
-  cudaMalloc(&cuda_C, size);
-  // Create a handle for CUBLAS
-  cublasHandle_t handle;
-  cublasCreate(&handle);
-
-  cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, N, N, N, &alpha, cuda_A, N,
-              cuda_B, N, &beta, cuda_C, N);
-
-  cudaMemcpy(C, cuda_C, size, cudaMemcpyDeviceToHost);
-
-  cudaFree(cuda_A);
-  cudaFree(cuda_B);
-  cudaFree(cuda_C);
-  cublasDestroy(handle);
-}
-
-void cublas_v3_1(int N, REAL *A, REAL *B, REAL *C) {
   int size = N * N * sizeof(REAL);
   REAL *cuda_A;
   REAL *cuda_B;
